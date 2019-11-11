47abfce0ed9155ed79d2227ca0cfd683c7849483
eduardosalaz <56232104+eduardosalaz@users.noreply.github.com>
2019-11-11 11:49:15 -0600

    Update pascal_cuadrado.pas

--- pascal_cuadrado.pas
+++ pascal_cuadrado.pas
@@ -5,22 +5,27 @@ procedure pascal(r, n : Integer);
   var
     i, c, k : Integer;
   begin
-    for i := 0 to r-1 do
-    begin
-      c := n;
-      for k := 0 to i do
-      begin
-        write(c:3);
-        write(' ');
-        c := ((c * (i-k)) div (k+1));
-      end;
-      writeln;
-   end;
-end;
+        for i := 0 to r-1 do
+            begin
+            c := n;
+            for k := 0 to i do
+                begin
+                write(c:3);
+                write(' ');
+                c := ((c * (i-k)) div (k+1));
+                end;
+            writeln;
+            end;
+    end;
  
 begin
 writeln ('Triangulo de Pascal, ingresar el numero de filas y la base del triangulo');
 readln( a, b);
-pascal(a, b);
-
+if a <> 0 then
+    if b <> 0 then
+        pascal(a, b)
+else
+    writeln('Ingresar filas y bases distintas a 0')
+    else
+    writeln('Ingresar filas y bases distintas a 0')
 end.
