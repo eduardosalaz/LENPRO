e9b5567e9865acb2e9c29e47f9439ded5cec3421
Eduardo Salazar <56232104+eduardosalaz@users.noreply.github.com>
2019-12-06 17:45:19 -0600

    Update djambi3.py

--- djambi3.py
+++ djambi3.py
@@ -9,6 +9,7 @@ terminar = 0
 poder=0
 contadorpoder=0
 contadorturnos=1
+
 '''
 MMN
 RPM
@@ -32,8 +33,9 @@ LAM
 '''
 Inicio del menu
 '''
-print("     Bienvenido a Djambi.")
-print(r"""
+print("     Bienvenido a Djambi.\n")
+def impresion_pantalla():
+    print(r"""
 ____   _                 _     _ 
 |  _ \(_) __ _ _ __ ___ | |__ (_)
 | | | | |/ _` | '_ ` _ \| '_ \| |
@@ -42,17 +44,12 @@ ____   _                 _     _
     |__/                         
 
 """)
-input("Presione la tecla intro para continuar...")
-clear()
 
 
+def mensaje_salida():
+    input("\nPresione la tecla intro para continuar...")
+    clear()
 
-
-
-
-'''
-Aqui empieze la impresion del tablero
-'''
 
 tablero=[]
  
@@ -127,26 +124,231 @@ tablero[8][9]=341
 tablero[9][7]=141
 tablero[9][8]=641
 tablero[9][9]=541
-'''
-Reglamento 
-'''
+#MOVIMIENTO DE MILITANTE
+impresion_pantalla()
+print("Objetivo: \nCapturar a los demás líderes enemigos con el uso de las diferentes piezas o estrategias entre jugadores como alianzas de manera informal. \n")
+print("Movimientos: \nCada jugador moverá una pieza en su turno, con la posibilidad de capturar en este movimiento a una ficha contraria. \nNinguna ficha puede saltar sobre otra (viva o muerta)\n")
+print("Capturas: \n Para capturar una pieza enemiga, debes posar una de las piezas que tienen permitido capturar, y colocar a la pieza muerta en una casilla distinta a la central. \n")
+mensaje_salida()
+impresion_pantalla()
+print("La letra M en el tablero Representa a los Militantes.\nLos militantes pueden moverse en las 8 direcciones marcadas, con un máximo de 2 casillas.\nNo pueden capturar al líder. \n")
+for x in range (0,10):
+    for y in range (0,10):
+        pieza=int(tablero[x][y]/100)   
+        color=int((tablero[x][y]%100)/10)
+        status=int(tablero[x][y]%10)
+        if (pieza==1):
+            if (color==1 and status==1 and x ==1):
+                print("\033[1;34;40m M", end ="")
+            elif (color==1 and status==1 and x !=1 and x!=2):
+                print("\033[1;37;40m 0", end ="")
+            elif (color==1 and status==1 and x !=1):
+                print("\033[1;36;40m 0", end ="")    
+            elif(color==2 and status==1):
+                print ("\033[1;37;40m 0", end ="")
+            elif(color==3 and status==1):
+                print ("\033[1;37;40m 0", end ="")   
+            elif(color==4 and status==1):
+                print ("\033[1;37;40m 0", end ="")
+            elif(status==0):
+                print ("\033[1;35;40m M", end ="") 
+        elif((x==1 and y==1)or(x==1 and y==2) or(x==1 and y==4) or(x==1 and y==5) or(x==2 and y==3)or(x==3 and y==3)):    
+             print ("\033[1;36;40m 0", end ="")               
+        elif (x==5 and y==5):
+            print ("\033[1;36;40m 0", end="")
+        elif (pieza!=1 and pieza !=0):
+            print ("\033[1;37;40m 0", end="")
+        elif(pieza==0):  
+            print("\033[1;37;40m",tablero[x][y], end="")  
+         
+        
+              
+    print("\033[1;37;40m\n")
+mensaje_salida()
+#MOVIMIENTO DE LIDER
+impresion_pantalla()
+print("La letra L en el tablero Representa a los lideres.\n Los líderes pueden moverse en las 8 direcciones marcadas, con un máximo de 8 casillas. \n")
+print("El líder es la pieza mas importante de Djambi, ya que tiene la capacidad de entrar a la casilla central, lo que le permite al jugador volver a hacer un movimiento después del turno de cada uno de sus adversarios. \n ")
+for x in range (0,10):
+    for y in range (0,10):
+        pieza=int(tablero[x][y]/100)   
+        color=int((tablero[x][y]%100)/10)
+        status=int(tablero[x][y]%10)
+        if (pieza==5):
+            if (color==1 and status==1):
+                print("\033[1;34;40m L", end ="")
+            elif(color==2 and status==1):
+                print ("\033[1;36;40m 0", end ="")
+            elif(color==3 and status==1):
+                print ("\033[1;36;40m 0", end ="")   
+            elif(color==4 and status==1):
+                print ("\033[1;36;40m 0", end ="")
+            elif(status==0):
+                print ("\033[1;37;40m 0", end ="")
+        elif(x==1 and y>1 and y<10):   
+            print("\033[1;36;40m 0", end="")
+        elif(y==1 and x>1 and x<10):   
+            print("\033[1;36;40m 0", end="") 
+        elif(x==y and x>1):
+            print("\033[1;36;40m 0", end="")                 
+        elif (x==5 and y==5):
+            print ("\033[1;36;40m ", end="")
+        elif (pieza!=5 and pieza !=0):
+            print ("\033[1;37;40m 0", end="")
+        elif(pieza==0):  
+            print("\033[1;37;40m",tablero[x][y], end="")  
+           
+        
+              
+    print("\033[1;37;40m\n")
+mensaje_salida()
+#MOVIMIENTO DE ASESINO
+impresion_pantalla()
+print("La letra A en el tablero Representa a los Asesinos.\nLos asesinos pueden moverse en las 8 direcciones marcadas, con un máximo de 8 casillas.\nCuando un asesino captura una pieza, se ve obligado a mover a dicha pieza a la posición original del asesino.\n")
+for x in range (0,10):
+    for y in range (0,10):
+        pieza=int(tablero[x][y]/100)   
+        color=int((tablero[x][y]%100)/10)
+        status=int(tablero[x][y]%10)
+        if (pieza==6):
+            if (color==1 and status==1):
+                print("\033[1;34;40m A", end ="")
+            elif(color==2 and status==1):
+                print ("\033[1;36;40m 0", end ="")
+            elif(color==3 and status==1):
+                print ("\033[1;36;40m 0", end ="")   
+            elif(color==4 and status==1):
+                print ("\033[1;37;40m 0", end ="")
+            elif(status==0):
+                print ("\033[1;35;40m 0", end ="")
+        elif(x==1 and y==0 or x==0 and y==1):
+            print("\033[1;37;40m",tablero[x][y], end="")    
+        elif((x==2 and y==1) or (x+1==y)):
+            print("\033[1;36;40m 0", end="")                
+        elif(x==1 and y>1 and y<10):   
+            print("\033[1;36;40m 0", end="")
+        elif(y==2 and x>1 or (x<2 and x>0)):   
+            print("\033[1;36;40m 0", end="")            
+        elif (x==5 and y==5):
+            print ("\033[1;36;40m 0", end="")
+        elif (pieza!=6 and pieza !=0):
+            print ("\033[1;37;40m 0", end="")
+        elif(pieza==0):  
+            print("\033[1;37;40m",tablero[x][y], end="")  
+           
+        
+              
+    print("\033[1;37;40m\n")
+mensaje_salida()
+#MOVIMIENTO DE NECROMÓVIL
+impresion_pantalla()
+print("La letra N en el tablero Representa a los Necromóviles.\nLos necromóviles pueden moverse en las 8 direcciones marcadas, con un máximo de 8 casillas.\nLos necromóviles no pueden capturar piezas, pero pueden mover piezas enemigas previamente capturadas\n")
+for x in range (0,10):
+    for y in range (0,10):
+        pieza=int(tablero[x][y]/100)   
+        color=int((tablero[x][y]%100)/10)
+        status=int(tablero[x][y]%10)
+        if (pieza==2):
+            if (color==1 and status==1):
+                print("\033[1;34;40m N", end ="")
+            elif(color==2 and status==1):
+                print ("\033[1;36;40m 0", end ="")
+            elif(color==3 and status==1):
+                print ("\033[1;36;40m 0", end ="")   
+            elif(color==4 and status==1):
+                print ("\033[1;36;40m 0", end ="")
+            elif(status==0):
+                print ("\033[1;35;40m N", end ="")
+        elif(x==2 and y==4 or x==1 and y==5 or x==4 and y==2 or x==5 and y==1):
+            print ("\033[1;36;40m 0", end ="")        
+        elif(x==y and x>0):
+            print ("\033[1;36;40m 0", end ="")
+        elif(x==3 and x>0 and y>0):
+            print ("\033[1;36;40m 0", end ="")
+        elif(y==3 and y>0 and x>0):
+            print ("\033[1;36;40m N", end ="")                    
+        elif (x==5 and y==5):
+            print ("\033[1;36;40m 0", end="")
+        elif (pieza!=2 and pieza !=0):
+            print ("\033[1;37;40m 0", end="")
+        elif(pieza==0):  
+            print("\033[1;37;40m",tablero[x][y], end="")  
+           
+        
+              
+    print("\033[1;37;40m\n")
+mensaje_salida()
+#MOVIMIENTO DE reportero
+impresion_pantalla()
+print("La letra R en el tablero Representa a los Reporteros.\nLos reporteros pueden moverse en las 8 direcciones marcadas, con un máximo de 8 casillas.\nLos reporteros no pueden capturar de manera diagonal y la pieza capturada permanece en su misma posición.\n")
+for x in range (0,10):
+    for y in range (0,10):
+        pieza=int(tablero[x][y]/100)   
+        color=int((tablero[x][y]%100)/10)
+        status=int(tablero[x][y]%10)
+        if (pieza==3):
+            if (color==1 and status==1):
+                print("\033[1;34;40m R", end ="")
+            elif(color==2 and status==1):
+                print ("\033[1;36;40m 0", end ="")
+            elif(color==3 and status==1):
+                print ("\033[1;36;40m 0", end ="")   
+            elif(color==4 and status==1):
+                print ("\033[1;37;40m 0", end ="")
+            elif(status==0):
+                print ("\033[1;35;40m 0", end ="")  
+        elif(x==1 and y==2 or (x-1==y and x>1)):
+            print ("\033[1;36;40m 0", end ="")
 
-print("El objetivo del juego es obtener el poder absoluto al ser el último jefe vivo a bordo. \n Aunque las alianzas informales se pueden acordar temporalmente, no hay equipo: cada jugador juega contra los otros jugadores.\n")
-print("Cada jugador mueve en su turno una de sus fichas pudiendo capturar una pieza adversaria o solo moverse a una casilla vacía.\n")
-print("Cuando un jugador mueve una pieza no puede saltar por encima de las demás piezas aliadas o adversarias.\n")
-print("Para capturar una pieza, se debe posicionar exactamente encima de la posición de dicha pieza a ser capturada, sin pasarse de casilla, con algunas excepciones.\n")
-print("Cuando un jugador captura una pieza enemiga puede moverla a una casilla vacía distinta a la casilla central, con algunas excepciones.\n")
-print("La casilla central solo puede estar ocupada por una pieza de tipo líder.\n")
-print("Cuando el líder de un equipo se encuentra en la casilla central, tiene derecho a un turno extra después de los turnos de los demás jugadores.\n")
-print("Líder: Puede moverse de manera horizontal, vertical o diagonal con un máximo de 8 casillas.\n")
-print("Militantes: Pueden moverse de manera horizontal, vertical o diagonal con un máximo de 2 casillas.\n")
-print("Asesino: Puede moverse de  manera horizontal, vertical o diagonal con un máximo de 8 casillas.\nCuando captura a una pieza, se ve obligado a mover dicha pieza capturada a la casilla de la posición original del asesino.\n")
-print("Reportero: Puede moverse de manera horizontal, vertical o diagonal con un máximo de 8 casillas.\nPara capturar a una pieza, debe de colocarse en una casilla adyacente de manera horizontal o vertical, no diagonal.\n")
-print("El provocador y necromóvil son las únicas 2 piezas que no pueden matar otras, pero transportan piezas enemigas vivas o capturadas.\n")
-print("Provocador: Puede moverse de manera horizontal, vertical o diagonal con un máximo de 8 casillas.\nEsta pieza no puede capturar piezas contrarias, pero puede mover piezas vivas, de una manera similar a la captura.\n")
-print("Necromóvil: Puede moverse de manera horizontal, vertical o diagonal con un máximo de 8 casillas.\nEsta pieza no puede capturar piezas contrarias, pero puede mover piezas previamente capturadas, de una manera similar a la captura.\n")
-print("\n")
-input("\033[1;37;40m Presione la tecla intro para iniciar el juego...")
+        elif(y==1 and x>0):
+            print ("\033[1;36;40m 0", end ="")
+        elif(x==2 and y>0):
+            print ("\033[1;36;40m 0", end ="")              
+        elif (x==5 and y==5):
+            print ("\033[1;36;40m 0", end="")
+        elif (pieza!=3 and pieza !=0):
+            print ("\033[1;37;40m 0", end="")
+        elif(pieza==0):  
+            print("\033[1;37;40m",tablero[x][y], end="")  
+           
+        
+              
+    print("\033[1;37;40m\n")
+mensaje_salida()
+#MOVIMIENTO DE provocador
+impresion_pantalla()
+print("La letra P en el tablero Representa a los Provocadores.\nLos provocadores pueden moverse en las 8 direcciones marcadas, con un máximo de 8 casillas.\nLos provocadores no pueden capturar piezas, pero pueden mover piezas enemigas que no han sido capturadas.\n")
+for x in range (0,10):
+    for y in range (0,10):
+        pieza=int(tablero[x][y]/100)   
+        color=int((tablero[x][y]%100)/10)
+        status=int(tablero[x][y]%10)
+        if (pieza==4):
+            if (color==1 and status==1):
+                print("\033[1;36;40m P", end ="")
+            elif(color==2 and status==1):
+                print ("\033[1;36;40m 0", end ="")
+            elif(color==3 and status==1):
+                print ("\033[1;36;40m 0", end ="")   
+            elif(color==4 and status==1):
+                print ("\033[1;36;40m 0", end ="")
+            elif(status==0):
+                print ("\033[1;35;40m 0", end ="") 
+        elif(x==y and x>0 or x==1 and y==3 or x==3 and y==1):
+            print ("\033[1;36;40m 0", end ="")          
+        elif(x==2 and y>0 or y==2 and x>0):
+            print ("\033[1;36;40m 0", end ="")         
+        elif (x==5 and y==5):
+            print ("\033[1;36;40m 0", end="")
+        elif (pieza!=4 and pieza !=0):
+            print ("\033[1;37;40m 0", end="")
+        elif(pieza==0):  
+            print("\033[1;37;40m",tablero[x][y], end="")  
+           
+        
+              
+    print("\033[1;37;40m\n")
+mensaje_salida()
 def actualizar():
     clear()
     print("L = Líder. M = Militante. A = Asesino. R = Reportero. P = Provocador. N = Necromóvil.   Solo se pueden ingresar numeros enteros del 1 al 9\n")
